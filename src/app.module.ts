2e03df8b932a9e8b2d0442d41cfd5e6bb08d3c31
Luka <luka@pop-os.localdomain>
2019-11-11 18:22:38 +0100

    consul

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
+import { BootModule } from '@nestcloud/boot';
+import { NEST_BOOT, NEST_CONSUL } from '@nestcloud/common';
+import { ConfigModule } from '@nestcloud/config';
+import { ConsulModule } from '@nestcloud/consul';
+import { ServiceModule } from '@nestcloud/service';
 import { Module } from '@nestjs/common';
+import { resolve } from 'path';
+
 import { UploadModule } from './modules/upload/upload.module';
 
 @Module({
-  imports: [UploadModule],
+  imports: [
+    UploadModule,
+    ConsulModule.register({ dependencies: [NEST_BOOT] }),
+    BootModule.register(resolve(), 'consul.yml'),
+    ConfigModule.register({ dependencies: [NEST_BOOT, NEST_CONSUL] }),
+    ServiceModule.register({ dependencies: [NEST_BOOT] }),
+  ],
 })
 export class AppModule {}
