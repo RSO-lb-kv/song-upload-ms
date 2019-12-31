8755204a3211fe540f98b5c89ae89bc95185e16b
Luka <luka@pop-os.localdomain>
2019-12-31 12:19:00 +0100

    add imageUrl to song

--- src/modules/upload/song.validation.ts
+++ src/modules/upload/song.validation.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class VSong {
   @IsString()
@@ -11,4 +11,9 @@ export class VSong {
   @IsString()
   @IsNotEmpty()
   uploadedBy: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  imageUrl: string;
 }
